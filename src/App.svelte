40c86ddb2348acadc2f33f39b7126517f384ef20
omer <anwar733649039@gmail.com>
2022-09-21 23:16:09 +0300

    search and render cities

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,30 @@
 <script>
   import Input from "./components/Input.svelte";
+  import data from './data/data.json'
+
+  let city = '';
+  let cities = []
+  
+  const search_for_city = (event) => {
+    const text = event.target.value
+    city = text
+    cities = data.filter(city => city.includes(text))
+  }
 </script>
 
-<main>
-  <!-- BG ovrelay -->
-  <div class="fixed inset-0 w-full h-screen z-0 bg-black bg-opacity-40"/>
+<!-- BG ovrelay -->
+<div class="absolute inset-0 w-full h-screen z-0 bg-black bg-opacity-40"/>
 
-  <Input placeholder='المدينة' />
+<main class="absolute inset-0 w-full h-screen z-50 flex items-center justify-center">  
+  <div class="w-96">
+    <Input on_input={search_for_city} placeholder='المدينة' />
+
+    {#if city}
+      <div class="bg-white mt-3 rounded-2xl max-h-72 overflow-y-auto p-2 z-50">
+        {#each cities as city}
+          <h1 class="px-6 py-2 mt-1 rounded-lg whitespace-nowrap overflow-hidden text-ellipsis hover:bg-gray-100 text-primary cursor-pointer">{city}</h1>
+        {/each}
+      </div>  
+    {/if}
+  </div>
 </main>
