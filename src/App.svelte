10a650c014d6c9399176f5f7d6b9d3480b3f9001
omer <anwar733649039@gmail.com>
2022-09-21 23:39:49 +0300

    choose city and make api link

--- src/App.svelte
+++ src/App.svelte
@@ -2,13 +2,20 @@
   import Input from "./components/Input.svelte";
   import data from './data/data.json'
 
-  let city = '';
+  let search = '';
   let cities = []
   
   const search_for_city = (event) => {
     const text = event.target.value
-    city = text
+    search = text
     cities = data.filter(city => city.includes(text))
+  }
+
+  const choose_city = (value) => {
+    search = value
+    cities = []
+    const [city,country] = value.split('-')
+    const link = `http://api.aladhan.com/v1/timingsByCity?city=${city}&country=${country}&method=8`
   }
 </script>
 
@@ -17,16 +24,16 @@
 
 <main class="absolute inset-0 w-full h-screen z-50 flex items-center justify-center">  
   <div class="w-96 relative">
-    <Input on_input={search_for_city} placeholder='المدينة' />
+    <Input on_input={search_for_city} value={search} placeholder='المدينة' />
 
-    {#if city && cities.length}
+    {#if search && cities.length}
       <div class="absolute top-16 w-full bg-white mt-3 transition-all rounded-2xl max-h-72 overflow-y-auto p-2 z-50">
         {#each cities as city}
-          <h1 class="px-6 py-2 mt-1 rounded-lg select-none whitespace-nowrap overflow-hidden text-ellipsis hover:bg-gray-100 text-primary cursor-pointer">{city}</h1>
+          <h1 on:click={() => choose_city(city)} class="px-6 py-2 mt-1 rounded-lg select-none whitespace-nowrap overflow-hidden text-ellipsis hover:bg-gray-100 text-primary cursor-pointer">{city}</h1>
         {/each}
       </div>  
     {/if}
-    {#if city && !cities.length}
+    {#if search && !data.includes(search) && !cities.length}
       <div class="absolute top-16 w-full bg-white mt-3 transition-all rounded-2xl max-h-72 overflow-y-auto p-2 z-50">
         <h1 class="px-6 py-2 mt-1 rounded-lg select-none whitespace-nowrap overflow-hidden text-ellipsis text-primary">
           لا يوجد مدينة بهذا الاسم
--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -1,8 +1,8 @@
 <script>
-    export let placeholder = '',icon = '',type='text', on_input = ()=>{};
+    export let placeholder = '',icon = '',type='text', value = '', on_input = ()=>{};
 </script>
 <div class="relative h-16 flex items-center">
-    <input on:input={on_input} type={type} placeholder={placeholder} class="h-full transition-all px-6 focus-visible:outline-none input w-full rounded-2xl bg-white py-2 text-primary text-xl"/>
+    <input on:input={on_input} type={type} placeholder={placeholder} value={value} class="h-full transition-all px-6 focus-visible:outline-none input w-full rounded-2xl bg-white py-2 text-primary text-xl"/>
     {#if icon}
         <img src={icon} alt="icon" class="absolute right-6 w-8 h-8"/>
     {/if}
