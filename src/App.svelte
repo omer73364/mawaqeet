420739aa1eb774408ebab0e1248d7c7e57fd95ec
omer <anwar733649039@gmail.com>
2022-09-22 01:08:07 +0300

    improve loading experince

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,7 @@
   let search = '';
   let cities = []
   let show_input = true
+  let loading = false
   
   const search_for_city = (event) => {
     const text = event.target.value
@@ -15,8 +16,10 @@
   }
 
   const get_data = async(link) => {
-    show_input = false
+    loading = true
     const result = await (await fetch(link)).json()
+    loading = false
+    show_input = false
     console.log(result)
   }
 
@@ -37,11 +40,16 @@
     
     {#if show_input}
       <div out:blur={{duration: 500}}>
-        <Input on_input={search_for_city} value={search} placeholder='المدينة' />
+        <Input 
+          placeholder='المدينة' 
+          value={search}
+          loading={loading} 
+          on_input={search_for_city} 
+        />
       </div>
     {/if}
 
-    {#if search && show_input}
+    {#if search && show_input && !loading}
       <div in:slide={{duration: 200}} out:slide={{duration: 200}} class={`absolute top-16 w-full bg-white mt-3 transition-all rounded-2xl ${cities.length ? 'h-72' : 'h-auto'} overflow-y-auto p-2 z-50`}>
         
         {#if cities?.length}
@@ -52,7 +60,6 @@
             defaultLoading={false}
           >
             <h1 on:click={() => choose_city(city)} class="px-6 py-2 mt-1 rounded-lg select-none whitespace-nowrap overflow-hidden text-ellipsis hover:bg-gray-100 text-primary cursor-pointer">{city}</h1>
-            
             <img slot="loading" src="/imgs/loading.svg" alt="loading" width="18"/>
           </LazyList>
         {:else}
