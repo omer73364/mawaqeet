f08b3c06b77c6c9be4ac55b9184ed6fc7d4882a2
omer <anwar733649039@gmail.com>
2022-09-21 23:24:35 +0300

    make cities aria absolute

--- src/App.svelte
+++ src/App.svelte
@@ -16,13 +16,13 @@
 <div class="absolute inset-0 w-full h-screen z-0 bg-black bg-opacity-40"/>
 
 <main class="absolute inset-0 w-full h-screen z-50 flex items-center justify-center">  
-  <div class="w-96">
+  <div class="w-96 relative">
     <Input on_input={search_for_city} placeholder='المدينة' />
 
-    {#if city}
-      <div class="bg-white mt-3 rounded-2xl max-h-72 overflow-y-auto p-2 z-50">
+    {#if city && cities.length}
+      <div class="absolute top-16 w-full bg-white mt-3 transition-all rounded-2xl max-h-72 overflow-y-auto p-2 z-50">
         {#each cities as city}
-          <h1 class="px-6 py-2 mt-1 rounded-lg whitespace-nowrap overflow-hidden text-ellipsis hover:bg-gray-100 text-primary cursor-pointer">{city}</h1>
+          <h1 class="px-6 py-2 mt-1 rounded-lg select-none whitespace-nowrap overflow-hidden text-ellipsis hover:bg-gray-100 text-primary cursor-pointer">{city}</h1>
         {/each}
       </div>  
     {/if}
