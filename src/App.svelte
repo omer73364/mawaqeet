729bb14d9d2e805e7e1c44f17ff60e854614b6ea
omer <anwar733649039@gmail.com>
2022-09-22 17:31:22 +0300

    شىهةشفهخى شىي يثسهلى هسسعثس

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
   import Input from "./components/Input.svelte";
-  import {blur,slide} from 'svelte/transition';
+  import {blur,fade,slide} from 'svelte/transition';
   import LazyList from 'lazy-load-list/svelte'
   import Loading from './components/Loading.svelte'
   import { onMount } from "svelte";
@@ -103,7 +103,7 @@
   <div class="w-96 relative">
   {#if mawaqeet_data && !show_input}
     <!-- Render data -->
-    <div in:slide={{duration:500,delay:800}} class="w-full rounded-2xl bg-primary py-6 px-8 flex flex-col relative">
+    <div in:slide={{duration:500,delay:800}} out:fade={{duration:500}} class="w-full rounded-2xl bg-primary py-6 px-8 flex flex-col relative">
       <img on:click={change_city} class="absolute cursor-pointer top-6 left-8" src="./imgs/pin.svg" alt="pin" width="30"/>
       <h1 class="text-white text-4xl">{format_time(time)}</h1>
       <h1 class="text-white text-6xl my-4">{mawaqeet_data.city}</h1>
@@ -111,40 +111,42 @@
       <h1 class="text-white opacity-80 text-2xl mb-1">{mawaqeet_data.date}</h1>
     </div>
   {:else}
-    <!-- Search input -->
-    {#if show_input && !is_getting_data}
-      <div out:blur={{duration: 500}}>
-        <h1 class="text-4xl text-white mb-4 text-center">مواقيت الصلاة</h1>
-        <Input 
-          placeholder='المدينة' 
-          value={search}
-          loading={is_loading}
-          on_input={search_for_city} 
-        />
-      </div>
-    {/if}
-    <!-- Select cities -->
-    {#if search && show_input && !is_loading}
-      <div in:slide={{duration: 200}} out:slide={{duration: 200}} class={`absolute top-[7.5rem] w-full bg-white mt-3 transition-all rounded-2xl ${cities.length >= 10 ? 'h-72' : 'h-auto'} overflow-y-auto p-2 z-50`}>
-        
-        {#if cities?.length}
-          <LazyList
-            data={cities}
-            itemsPerRender={10}
-            let:item={city}
-            defaultLoading={false}
-          >
-            <h1 on:click={() => choose_city(city)} class="px-6 py-2 mt-1 rounded-lg select-none whitespace-nowrap overflow-hidden text-ellipsis hover:bg-gray-100 text-primary cursor-pointer">{city}</h1>
-            <img slot="loading" src="/imgs/loading.svg" alt="loading" width="18"/>
-          </LazyList>
-        {:else}
-          <h1 class="px-6 py-2 mt-1 rounded-lg select-none whitespace-nowrap overflow-hidden text-ellipsis text-primary">
-            لا يوجد مدينة بهذا الاسم
-          </h1>
-        {/if}
+    <div in:blur={{duration: 500,delay:600}}>
+      <!-- Search input -->
+      {#if show_input && !is_getting_data}
+        <div out:blur={{duration: 500}} in:blur={{duration: 500}}>
+          <h1 class="text-4xl text-white mb-4 text-center">مواقيت الصلاة</h1>
+          <Input 
+            placeholder='المدينة' 
+            value={search}
+            loading={is_loading}
+            on_input={search_for_city} 
+          />
+        </div>
+      {/if}
+      <!-- Select cities -->
+      {#if search && show_input && !is_loading}
+        <div in:slide={{duration: 200}} out:slide={{duration: 200}} class={`absolute top-[7.5rem] w-full bg-white mt-3 transition-all rounded-2xl ${cities.length >= 10 ? 'h-72' : 'h-auto'} overflow-y-auto p-2 z-50`}>
+          
+          {#if cities?.length}
+            <LazyList
+              data={cities}
+              itemsPerRender={10}
+              let:item={city}
+              defaultLoading={false}
+            >
+              <h1 on:click={() => choose_city(city)} class="px-6 py-2 mt-1 rounded-lg select-none whitespace-nowrap overflow-hidden text-ellipsis hover:bg-gray-100 text-primary cursor-pointer">{city}</h1>
+              <img slot="loading" src="/imgs/loading.svg" alt="loading" width="18"/>
+            </LazyList>
+          {:else}
+            <h1 class="px-6 py-2 mt-1 rounded-lg select-none whitespace-nowrap overflow-hidden text-ellipsis text-primary">
+              لا يوجد مدينة بهذا الاسم
+            </h1>
+          {/if}
 
-      </div>  
-    {/if}
+        </div>  
+      {/if}
+    </div>
   {/if}
   </div>
 </main>
